cc0c74a07f3f3897b90675b7d1a3b45eda9dbd84
Leonardo Galindo <leonardogalindo8@gmail.com>
2022-11-17 11:12:53 -0300

    Atualizações

--- index.js
+++ index.js
@@ -23,23 +23,32 @@ form.addEventListener('submit', function (e) {
 })
 
 const setResp = (msg) => {
-    resp.innerHTML = ''
+    limpaResp(resp)
     let p = document.createElement('p')
+    p.classList.add('respCalc')
     p.innerHTML = msg
     resp.appendChild(p)
 }
 
 const calcularSalMasc = (salario) => {
+    limpaResp(resp)
     let p = document.createElement('p')
     salarioATT = salario + (salario * 0.205)
-    p.innerHTML = `Se você fosse um homem seu salário seria de R$ ${salarioATT}. Você ganharia 20.5% a mais.`
+    p.classList.add('respCalc')
+    p.innerHTML = `Se você fosse um homem seu salário seria de <span> R$ ${salarioATT} </span>. Você ganharia 20.5% a mais.`
     resp.appendChild(p)
+    
 }
 
 const calcularSalFem = (salario) => {
+    limpaResp(resp)
     let p = document.createElement('p')
     salarioATT = salario - (salario * 0.205)
-    p.innerHTML = `Se você fosse uma mulher seu salário seria de R$ ${salarioATT}. Você ganharia 20.5% a menos.`
+    p.classList.add('respCalc')
+    p.innerHTML = `Se você fosse uma mulher seu salário seria de <span>R$ ${salarioATT}</span>. Você ganharia 20.5% a menos.`
     resp.appendChild(p)
 }
 
+const limpaResp = (resp) => {
+    resp.innerHTML = ''
+}
